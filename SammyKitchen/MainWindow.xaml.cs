6e4e76f545db7decef3a6e065db4a650e65b114d
LONELY-WOLF <zvenayte@pisem.net>
2014-01-11 08:47:17 +0400

    Init

---
+++ SammyKitchen/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using sam_unpack_lib;
+
+namespace SammyKitchen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "SMD files (*.smd, *.wp8, *.csc)|*.smd;*.wp8;*.csc|MBN files (*.mbn)|*.mbn";
+            dlg.Multiselect = false;
+            dlg.AddExtension = true;
+            dlg.ShowDialog();
+            List<SMD.Section> sections = SMD.GetSections(dlg.FileName, true);
+            lbxSections.DataContext = sections;
+        }
+    }
+}
